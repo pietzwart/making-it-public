a8a1f5343a0bdc9302cb8e64c65377d35b393ecd
Franz-Kafka <spam@xj6652.de>
2016-01-28 18:22:39 +0100

    new version

--- book/generatebook.sh
+++ book/generatebook.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-pandoc --from html --to epub3 --self-contained --epub-chapter-level=1 --epub-stylesheet=styles.css --epub-cover-image=epubcover.jpg --epub-metadata=meta.xml --toc-depth=1 --output=ashes.epub title.html \
+pandoc --from html --to epub3 \
+--self-contained \
+--standalone \
+--epub-chapter-level=1 \
+--epub-stylesheet=styles.css \
+--epub-cover-image=assets/epubcover.jpg \
+--epub-metadata=meta.xml \
+--toc-depth=1 \
+--include-in-header=assets/jquery-1.11.3.min.js \
+--include-in-header=assets/script.js \
+--output=ashes.epub title.html \
 report1.html \
 chapter1.html 
 
